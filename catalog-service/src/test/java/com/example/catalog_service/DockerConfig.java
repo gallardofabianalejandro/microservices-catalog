ff93ff768d86aaf6c63ffe8d57d13d762bfa820e
fabian gallardo <gallardo_Fabian@hotmail.com>
2024-11-20 17:57:46 -0300

    commit files

---
+++ catalog-service/src/test/java/com/example/catalog_service/DockerConfig.java
@@ -0,0 +1,8 @@
+package com.example.catalog_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class DockerConfig {
+
+}
